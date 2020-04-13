6177c27b8948810c3af45a8b53f14c664cdc7799
Ninja243 <mweyaruider@gmail.com>
2020-04-13 09:02:09 +0200

    Init

---
+++ consume-rest/src/main/java/com/mweya/consumerest/ConsumingRestDemoApplication.java
@@ -0,0 +1,13 @@
+package com.mweya.consumerest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumingRestDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumingRestDemoApplication.class, args);
+	}
+
+}
